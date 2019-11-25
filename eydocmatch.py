8e3bc642f79c601909e96d11bdbb7fba676b40b7
drkarim <rashed.karim@gmail.com>
2019-11-25 12:23:22 +0100

    can rotate single docs

--- eydocmatch.py
+++ eydocmatch.py
@@ -13,17 +13,25 @@ ap.add_argument("-t", "--transformation", required=False,
                 help="specify which transformation 1=Rotate, 2=Translate")
 ap.add_argument("-p", "--rotate", required=False,
                 help="specify transformation parameter, for e.g. -p 90 for 90 degrees")
+ap.add_argument('--batch', nargs='?', help='Perform multiple different rotations in a range')
+
 
 args = vars(ap.parse_args())
 input_doc_path = args["image"]
 output_doc_path = args["output"]
 
 if 'transformation' in args and 'rotate' in args:
+    # Extract transformation params
     which_transform = args['transformation']
     rotate_angle = args['rotate']
     transform_param = dict()
     transform_param['angle'] = rotate_angle
+
+    # Execute
     bill = Bills(input_doc_path)
     bill.transform_doc(which_transform, transform_param)
-    bill.write_doc(output_doc_path)
+    bill.write_doc('transform',output_doc_path)
+
+#if 'batch' in args and 'angle_range' in args:
+
 
