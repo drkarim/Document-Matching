4e2c9bfb778be23f6caa0f342789f6b0b366e622
drkarim <rashed.karim@gmail.com>
2019-11-25 13:14:42 +0100

    can rotate N degrees from command line

--- Bills.py
+++ Bills.py
@@ -2,6 +2,10 @@ import imutils
 import numpy as np
 import pandas as pd
 import cv2
+import os
+import random
+import string
+
 
 
 class Bills:
@@ -12,8 +16,10 @@ class Bills:
         # load the image from disk
         self.image = cv2.imread(self.path_to_file)
 
-    def transform_doc(self, which_transform, transfom_param):
-        angle = int(transfom_param['angle'])
+    def transform_doc(self, which_transform, transform_param):
+
+        if 'angle' in transform_param:
+            angle = int(transform_param['angle'])
 
         if which_transform == 'rotate':
             self.image_transform = imutils.rotate(self.image, angle)
@@ -25,3 +31,24 @@ class Bills:
     def write_doc(self, which_doc, write_path):
         if which_doc == 'transform':
             cv2.imwrite(write_path, self.image_transform)
+
+    '''
+        Rotates the document at multiple angles, and 
+        writes each rotates image to file   
+    '''
+    def transform_doc_batch_angles(self,  angles, write_path):
+
+        which_transform = 'rotate_bound'        # rotates without cutting the document
+        transform_param = dict()
+        transform_param['angle'] = 0
+        # rotate doc at multiple angles and write to file
+        for angle in angles:
+            transform_param['angle'] = angle
+            self.transform_doc('rotate_bound', transform_param)
+            new_filename = self.append_num_to_filename(write_path, angle)
+            self.write_doc('transform',  new_filename)
+
+    # https://stackoverflow.com/questions/37487758/how-to-add-an-id-to-filename-before-extension
+    def append_num_to_filename(self, filename, num):
+        name, ext = os.path.splitext(filename)
+        return "{name}_{uid}{ext}".format(name=name, uid=num, ext=ext)
--- eydocmatch.py
+++ eydocmatch.py
@@ -1,8 +1,20 @@
 import argparse
 import cv2
+import re
 
 # construct the argument parse and parse the arguments
 from Bills import Bills
+
+
+def parseNumList(string):
+    m = re.match(r'(\d+)(?:-(\d+))?$', string)
+    # ^ (or use .split('-'). anyway you like.)
+    if not m:
+        raise ArgumentTypeError("'" + string + "' is not a range of number. Expected forms like '0-5' or '2'.")
+    start = m.group(1)
+    end = m.group(2) or start
+    return list(range(int(start, 10), int(end, 10) + 1))
+
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
@@ -13,8 +25,7 @@ ap.add_argument("-t", "--transformation", required=False,
                 help="specify which transformation 1=Rotate, 2=Translate")
 ap.add_argument("-p", "--rotate", required=False,
                 help="specify transformation parameter, for e.g. -p 90 for 90 degrees")
-ap.add_argument('--batch', nargs='?', help='Perform multiple different rotations in a range')
-
+ap.add_argument('--batch', type=parseNumList, help='Perform multiple different rotations in a range')
 
 args = vars(ap.parse_args())
 input_doc_path = args["image"]
@@ -22,16 +33,21 @@ output_doc_path = args["output"]
 
 if 'transformation' in args and 'rotate' in args:
     # Extract transformation params
-    which_transform = args['transformation']
-    rotate_angle = args['rotate']
-    transform_param = dict()
-    transform_param['angle'] = rotate_angle
+    if args['transformation'] is not None and args['rotate'] is not None:
+        which_transform = args['transformation']
+        rotate_angle = args['rotate']
+        transform_param = dict()
+        transform_param['angle'] = rotate_angle
 
-    # Execute
+        # Execute
+        bill = Bills(input_doc_path)
+        bill.transform_doc(which_transform, transform_param)
+        bill.write_doc('transform', output_doc_path)
+
+# if 'batch' in args and 'angle_range' in args:
+if 'batch' in args:
+    angles = args['batch']
+
     bill = Bills(input_doc_path)
-    bill.transform_doc(which_transform, transform_param)
-    bill.write_doc('transform',output_doc_path)
-
-#if 'batch' in args and 'angle_range' in args:
-
+    bill.transform_doc_batch_angles(angles, output_doc_path)
 
